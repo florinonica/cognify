7ef2f522bf9a499f2719d53513854c58ce949df8
florinonica <onicaflorin96@gmail.com>
2018-12-11 14:19:21 +0200

    small fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   #before_action :authenticate_user!
   layout "application"
+
+  def save_attachments(container, files)
+
+  	unless files.nil?
+  	  files.each do |file|
+  	    @attachment = container.attachments.new(user_id: current_user.id, file: file)
+
+  	    if @attachment.save
+  	      container.attachments << @attachment
+  	    end
+  	  end
+  	end
+  end
 end
