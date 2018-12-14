d0fc872fd217435fa9063b8a207a915364dc8a35
florinonica <onicaflorin96@gmail.com>
2018-12-14 10:13:13 +0200

    solved display issue

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,16 +3,17 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+
   #before_action :account_update_params, only: [:update]
-  
+
   # GET /resource/sign_up
   # def new
-  #   super
+  #  super
   # end
 
   # POST /resource
   # def create
-  #   super
+  #  super
   # end
 
   # GET /resource/edit
@@ -43,7 +44,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :nickname, :position, :type])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :type])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,4 @@
 class WelcomeController < ApplicationController
-  layout "application", except: [:index]
-
   def index
   end
 
