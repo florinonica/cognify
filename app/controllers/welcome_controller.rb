dff61e379aacca2eb1cc1f5e64da717c5e710bc2
florinonica <onicaflorin96@gmail.com>
2018-12-14 07:42:54 +0200

    Custom layout for index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,10 @@
 class WelcomeController < ApplicationController
+  layout "application", except: [:index]
 
   def index
   end
 
   def temp_index
   end
-  
+
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -5,4 +5,5 @@ class Course < ApplicationRecord
   #belongs_to :teacher
   #has_and_belongs_to_many :teacher_assistants, join_table: :teacher_assistants_courses
   #has_and_belongs_to_many :students, join_table: :students_courses
+  #
 end
