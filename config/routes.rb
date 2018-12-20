8ef164c31a3dac8ef1c58423e326df3dabe8accd
florinonica <onicaflorin96@gmail.com>
2018-12-20 08:38:43 +0200

    small ui fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :users, :controllers => { sessions: "users/sessions", registrations: 'users/registrations' }
   resources :categories
-  resources :courses
+  resources :courses do
+    resources :course_modules
+  end
   resources :users
 
   get "/temp" => "welcome#temp_index"
