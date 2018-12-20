83bf6774ba84853237101404d6952909d669f4e8
florinonica <onicaflorin96@gmail.com>
2018-12-20 11:20:29 +0200

    added permissions for admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,40 @@ class Admin < User
   def self.model_name
     User.model_name
   end
+
+  def can_create_category?
+    true
+  end
+
+  def can_edit_category?(category)
+    true
+  end
+
+  def can_delete_category?(category)
+    true
+  end
+
+  def can_create_course?
+    true
+  end
+
+  def can_edit_course?(course)
+    true
+  end
+
+  def can_delete_course?(course)
+    true
+  end
+
+  def can_create_module?
+    true
+  end
+
+  def can_edit_module?(course_module)
+    true
+  end
+
+  def can_delete_module?(course_module)
+    true
+  end
 end
