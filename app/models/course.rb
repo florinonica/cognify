1f94865e7a2e11ff820c307a491b01d78045bd24
florinonica <onicaflorin96@gmail.com>
2019-01-10 13:01:25 +0200

    removed teacher from course for the moment

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ApplicationRecord
   belongs_to :category
   has_many :attachments, :as => :container, dependent: :destroy
   has_many :course_modules, dependent: :destroy
-  belongs_to :teacher
+  #belongs_to :teacher
   has_and_belongs_to_many :teacher_assistants, join_table: :teacher_assistants_courses
   has_and_belongs_to_many :students, join_table: :enrollments
 end
--- db/migrate/20190110093448_drop_students_courses_table.rb
+++
@@ -1,5 +0,0 @@
-class DropStudentsCoursesTable < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :courses_students
-  end
-end
