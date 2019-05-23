6aa90a6cf55859b47d9c4c9486df68188b78d425
florinonica <onicaflorin96@gmail.com>
2019-05-23 15:34:31 +0300

    controllers

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -13,18 +13,47 @@ class AssignmentsController < ApplicationController
   end
 
   def create
+    @assingment = Assingment.new(assingment_params)
+    params.require(:course).permit(:files => [])
+    save_attachments(@assignment, params[:course][:files])
+
+    if @assingment.save
+      redirect_to assingments_path
+    else
+      @assingment.attachments.each do |file|
+        file.destroy
+      end
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    params.require(:assingment).permit(:files => [])
+    save_attachments(@assingment, params[:course][:files])
+
+    if @assingment.update(assingment_params)
+      redirect_to assingments_path
+    else
+      render 'edit'
+    end
+  end
 
   def destroy
+    @assignment.destroy
+    redirect_to :back
   end
 
   private
 
   def assignment_params
+    params.require(:assignment).permit(:container, :due_date)
   end
+
+  def get_assignment
+    @assignment = Assignment.find(params[:id])
+  end
+
 end
--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,5 +1,7 @@
 class GradesController < ApplicationController
   before_action :authenticate_user!
+  before_action :get_user, only: [:index, :new, :create, :edit, :update]
+  before_action :get_grade: [:show, :edit, :update, :destroy]
 
   def index
     @grades = @user.grades
@@ -13,18 +15,42 @@ class GradesController < ApplicationController
   end
 
   def create
+    @grade = @user.grades.new(grade_params)
+
+    if @grade.save
+      redirect_to user_grades_path(@user)
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @grade.update(grade_params)
+      redirect_to user_grades_path(@user)
+    else
+      render 'new'
+    end
+  end
 
   def destroy
+    @grade.destroy
+    redirect_to user_grades_path(@user)
   end
 
   private
 
   def grade_params
+    params.require(:grade).permit(:value, :container, :date)
+  end
+
+  def get_grade
+    @grade = Grade.find(params[:id])
+  end
+
+  def get_user
+    @user = User.find(params[:user_id])
   end
 end
