d1137830a073b0cf06750d481d65d660491acb95
florinonica <onicaflorin96@gmail.com>
2019-08-12 11:52:40 +0300

    added js for filtering

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,3 +25,68 @@ $(document).ready(function () {
     });
 
 });
+
+function startTime() {
+  var today = new Date();
+  var d = today.getDate();
+  var mo = today.getMonth() + 1;
+  var y = today.getFullYear();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =
+  d + "-" + mo + "-" + y + " " + h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};
+  return i;
+}
+
+function display(i) {
+  var x = document.getElementById("add");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+};
+
+function scrollBoard() {
+  var element = document.getElementById("board");
+  if (typeof(element) != 'undefined' && element != null) {
+    if((element.scrollHeight - element.clientHeight) <= element.scrollTop){
+      element.scrollTop = 0;
+    }
+  }
+};
+
+/*setInterval(scrollBoard,10000);
+
+$(document).on('turbolinks:load', function() {
+  $('#selectAll').click(function() {
+     if (this.checked) {
+         $('.c1').each(function() {
+             this.checked = true;
+         });
+     } else {
+        $('.c1').each(function() {
+             this.checked = false;
+         });
+     }
+  });
+  $('#selectAll1').click(function() {
+     if (this.checked) {
+         $('.c2').each(function() {
+             this.checked = true;
+         });
+     } else {
+        $('.c2').each(function() {
+             this.checked = false;
+         });
+     }
+  });
+});*/
